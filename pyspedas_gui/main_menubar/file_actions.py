1e2c913d2389e99b66c511a47affa9ce4dd37879
Jim Lewis <jwl@ssl.berkeley.edu>
2023-09-07 17:05:35 -0700

    Renamed parameters with PEP 8 conventions

--- pyspedas_gui/main_menubar/file_actions.py
+++ pyspedas_gui/main_menubar/file_actions.py
@@ -1,6 +1,6 @@
 from PySide6.QtGui import QAction
-def add_file_actions(mainWindow,fileMenu):
+def add_file_actions(main_window, file_menu):
     # Add "Exit" action to the "File" menu
-    exit_action = QAction("Exit", mainWindow)
-    exit_action.triggered.connect(mainWindow.close)
-    fileMenu.addAction(exit_action)
+    exit_action = QAction("Exit", main_window)
+    exit_action.triggered.connect(main_window.close)
+    file_menu.addAction(exit_action)
